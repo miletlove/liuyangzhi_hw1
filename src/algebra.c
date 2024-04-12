73172c8e900fa9136d81d237a09af6865ab57dcc
lyz <3068126392@qq.com>
2024-04-12 21:07:33 +0800

    liuyangzhihw1

--- src/algebra.c
+++ src/algebra.c
@@ -2,7 +2,7 @@
  * @Author: lyz 3068126392@qq.com
  * @Date: 2024-04-09 20:41:44
  * @LastEditors: lyz 3068126392@qq.com
- * @LastEditTime: 2024-04-12 20:06:26
+ * @LastEditTime: 2024-04-12 21:05:44
  * @FilePath: \c++e:\Workspace\liuyangzhi_hw1\src\algebra.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,138 +20,147 @@ Matrix create_matrix(int row, int col)
 
 Matrix add_matrix(Matrix a, Matrix b)
 {
-   
-        Matrix c=create_matrix(a.rows,a.cols);
-        if (a.rows == b.rows && a.cols == b.cols)
-        {
-            int i = 0, j = 0;
-            for (i = 0; i < a.rows; i++)
-            {
-                for (j = 0; j < a.cols; j++)
-                {
-                    c.data[i][j] = a.data[i][j] + b.data[i][j];
-                }
-            } // ToDo finished
-            return c;
-        }
-        else
-        {
-            printf("Error: Matrix a and b must have the same rows and cols.\n");
-            return create_matrix(0,0);
-        }
-        
-    }
-
-    Matrix sub_matrix(Matrix a, Matrix b)
-    {   Matrix c=create_matrix(a.rows,a.cols);
-        if (a.rows == b.rows && b.cols == a.cols)
-        {
-           
-            int i = 0, j = 0;
-            for (i = 0; i < a.rows; i++)
-            {
-                for (j = 0; j < a.cols; j++)
-                {
-                    c.data[i][j] = a.data[i][j] - b.data[i][j];
-                }
-            }
-            // ToDo finished
-            return c;
-        }
-        else
-        {
-            printf("Error: Matrix a and b must have the same rows and cols.\n");
-            return create_matrix(0,0);
-        }
-        
-    }
-
-    Matrix mul_matrix(Matrix a, Matrix b)
-    {   Matrix c=create_matrix(a.rows,a.cols);
-        if (a.cols != b.rows)
-        {
-            printf("Error:Matrix a's cols must be the same with b's rows.\n");
-            return create_matrix(0,0);
-        }
-        else
-        {
-           
-            int i, j;
-            for (i = 0; i < a.rows; i++)
-            {
-                for (j = 0; j < b.cols; j++)
-                {
-                    int k = 0;
-                    while (k < b.rows)
-                    {
-                        c.data[i][j] += a.data[i][k] * b.data[k][j];
-                        k++;
-                    }
-                }
-            }
-            return create_matrix(0,0);
-        }
-        // ToDo finished
-    }
-
-    Matrix scale_matrix(Matrix a, double k)
+    Matrix c = create_matrix(a.rows, a.cols);
+    if (a.rows == b.rows && a.cols == b.cols)
     {
-       Matrix c=create_matrix(a.rows,a.cols);
         int i = 0, j = 0;
         for (i = 0; i < a.rows; i++)
         {
             for (j = 0; j < a.cols; j++)
             {
-                c.data[i][j] = k * a.data[i][j];
+                c.data[i][j] = a.data[i][j] + b.data[i][j];
             }
-        }
-        // ToDo finished
+        } // ToDo finished
         return c;
     }
-
-    Matrix transpose_matrix(Matrix a)
-    {    Matrix c=create_matrix(a.cols,a.rows);
-        // ToDo
+    else
+    {
+        printf("Error: Matrix a and b must have the same rows and cols.\n");
         return create_matrix(0, 0);
     }
+}
 
-    double det_matrix(Matrix a)
+Matrix sub_matrix(Matrix a, Matrix b)
+{
+  Matrix c = create_matrix(a.rows, a.cols);
+    if (a.rows == b.rows && a.cols == b.cols)
     {
-        // ToDo
-        return 0;
-    }
-
-    Matrix inv_matrix(Matrix a)
-    {
-        // ToDo
-        return create_matrix(0, 0);
-    }
-
-    int rank_matrix(Matrix a)
-    {
-        // ToDo
-        return 0;
-    }
-
-    double trace_matrix(Matrix a)
-    {   int k;
-        int i,j=0;
-        for(i=0;i<a.rows;i++){
-            k+=a.data[i][i];
-        }
-        // ToDo finished
-        return k;
-    }
-
-    void print_matrix(Matrix a)
-    {
-        for (int i = 0; i < a.rows; i++)
+        int i = 0, j = 0;
+        for (i = 0; i < a.rows; i++)
         {
-            for (int j = 0; j < a.cols; j++)
+            for (j = 0; j < a.cols; j++)
             {
-                // 按行打印，每个元素占8个字符的宽度，小数点后保留2位，左对齐
-                printf("%-8.2f", a.data[i][j]);
+                c.data[i][j] = a.data[i][j] - b.data[i][j];
             }
-            printf("\n");
+        } // ToDo finished
+        return c;
+    }
+    else
+    {
+        printf("Error: Matrix a and b must have the same rows and cols.\n");
+        return create_matrix(0, 0);
+    }
+}
+
+Matrix mul_matrix(Matrix a, Matrix b)
+{
+    Matrix c = create_matrix(a.rows, b.cols);
+    if (a.cols != b.rows)
+    {
+        printf("Error:Matrix a's cols must be the same with b's rows.\n");
+        return create_matrix(0, 0);
+    }
+    else
+    {
+        int i, j;
+        for (i = 0; i < a.rows; i++)
+        {
+            for (j = 0; j < b.cols; j++)
+            {   c.data[i][j]=0;
+                int k = 0;
+                while (k < b.rows)
+                {
+                    c.data[i][j] =c.data[i][j]+ a.data[i][k] * b.data[k][j];
+                    k++;
+                }
+            }
+        }
+        return c;
+    }
+    // ToDo finished
+}
+
+Matrix scale_matrix(Matrix a, double k)
+{
+    Matrix c = create_matrix(a.rows, a.cols);
+    int i = 0, j = 0;
+    for (i = 0; i < a.rows; i++)
+    {
+        for (j = 0; j < a.cols; j++)
+        {
+            c.data[i][j] = k * a.data[i][j];
         }
     }
+    // ToDo finished
+    return c;
+}
+
+Matrix transpose_matrix(Matrix a)
+{
+    Matrix c = create_matrix(a.cols, a.rows);
+    int i,j=0;
+    for(i=0;i<a.rows;i++){
+        for(j=0;j<a.cols;j++){
+            c.data[j][i]=a.data[i][j];
+        }
+    }
+    // ToDo finishef
+    return c;
+}
+
+double det_matrix(Matrix a)
+{
+    // ToDo
+    return 0;
+}
+
+Matrix inv_matrix(Matrix a)
+{
+    // ToDo
+    return create_matrix(0, 0);
+}
+
+int rank_matrix(Matrix a)
+{
+    // ToDo
+    return 0;
+}
+
+double trace_matrix(Matrix a)
+{   if(a.rows != a.cols){
+    printf("Matrix a's rows must be the same as its cols.\n");
+    return 0;
+}else{
+    int k=0;
+    int i, j = 0;
+    for (i = 0; i < a.rows; i++)
+    {
+        k += a.data[i][i];
+    }
+    // ToDo finished
+    return k;
+}
+}
+
+void print_matrix(Matrix a)
+{
+    for (int i = 0; i < a.rows; i++)
+    {
+        for (int j = 0; j < a.cols; j++)
+        {
+            // 按行打印，每个元素占8个字符的宽度，小数点后保留2位，左对齐
+            printf("%-8.2f", a.data[i][j]);
+        }
+        printf("\n");
+    }
+}
